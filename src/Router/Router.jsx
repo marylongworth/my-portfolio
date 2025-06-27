d2c74c868ac7f3e0394c2fc6477de99d582fe678
Mary Longworth <appglutenfree@gmail.com>
2025-06-27 12:35:54 +0100

    fix  refresh

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import Root from "../Layouts/Root";
 import Home from "../Pages/Home";
 import AllServices from "../Pages/AllServices";
@@ -9,7 +9,8 @@ import AllBlogs from "../Pages/AllBlogs/AllBlogs";
 import BlogDetails from "../Pages/BlogDetails/BlogDetails";
 import Portfolio from "../Pages/Portfolio/Portfolio";
 import PortfolioDetails from "../Pages/PortfolioDetails/PortfolioDetails";
-// portfolio pages
+
+// Portfolio detail pages
 import GlugoDetails from "../Pages/PortfolioDetails/GlugoDetails";
 import GfggDetails from "../Pages/PortfolioDetails/GfggDetails";
 import TpDetails from "../Pages/PortfolioDetails/TpDetails";
@@ -17,31 +18,31 @@ import PprDetails from "../Pages/PortfolioDetails/PprDetails";
 import DandWDetails from "../Pages/PortfolioDetails/DandWDetails";
 import GfretreatsDetails from "../Pages/PortfolioDetails/GfretreatsDetails";
 
-export const router = createBrowserRouter(
+export const router = createHashRouter(
   [
     {
       path: "/",
       element: <Root />,
       children: [
         { path: "/", element: <Home /> },
-        { path: "/all-services", element: <AllServices /> },
-        { path: "/service-details", element: <ServiceDetails /> },
-        { path: "/all-blog", element: <AllBlogs /> },
-        { path: "/blog-details", element: <BlogDetails /> },
-        { path: "/contact", element: <Contact /> },
-        { path: "/footer", element: <Footer /> },
-        { path: "/portfolio", element: <Portfolio /> },
-        { path: "/portfolio-details", element: <PortfolioDetails /> },
-        { path: "/glugo-details", element: <GlugoDetails /> },
-        { path: "/gfgg-details", element: <GfggDetails /> },
-        { path: "/ppr-details", element: <PprDetails /> },
-        { path: "/tp-details", element: <TpDetails /> },
-        { path: "/dandw-details", element: <DandWDetails /> },
-        { path: "/gfretreats-details", element: <GfretreatsDetails /> },
+        { path: "all-services", element: <AllServices /> },
+        { path: "service-details", element: <ServiceDetails /> },
+        { path: "all-blog", element: <AllBlogs /> },
+        { path: "blog-details", element: <BlogDetails /> },
+        { path: "contact", element: <Contact /> },
+        { path: "footer", element: <Footer /> },
+        { path: "portfolio", element: <Portfolio /> },
+        { path: "portfolio-details", element: <PortfolioDetails /> },
+        { path: "glugo-details", element: <GlugoDetails /> },
+        { path: "gfgg-details", element: <GfggDetails /> },
+        { path: "ppr-details", element: <PprDetails /> },
+        { path: "tp-details", element: <TpDetails /> },
+        { path: "dandw-details", element: <DandWDetails /> },
+        { path: "gfretreats-details", element: <GfretreatsDetails /> },
       ],
     },
   ],
   {
-    basename: "/my-portfolio", 
+    basename: "/my-portfolio",
   }
 );

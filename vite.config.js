10c2a38a7b3994c8dd4783db873e7120d6e42462
Mary Longworth <appglutenfree@gmail.com>
2025-06-27 11:03:02 +0100

    code to deploy to github pages

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-// const path = require('path')
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/my-portfolio', 
   plugins: [react()],
-  // root:path.resolve(__dirname, 'src'),
-  // build:{
-  //   outDir:"../dist"
-  // }
 })
